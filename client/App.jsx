ab2077a4ec62b7d5b6a6693f6752046a6f2b3875
Matthew Salvador <matthew.jsalvador@gmail.com>
2021-03-03 00:41:02 -0500

    webpack is set up

---
+++ client/App.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import { render } from 'react-dom';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello World?1234????!</h1>
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+const PORT = 3000;
+
+// const apiRouter = require('./routes/api');
+
+app.use('/build', express.static(path.join(__dirname, '../build')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/index.html'));
+})
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}...`);
+});
+
+module.export = app;
